2f77b103671acfff10483263e32d674740102d74
Marco-Villanueva-21 <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-04-19 12:54:53 +0000

    removed commented-out code and added space for more clarity

--- sweep.py
+++ sweep.py
@@ -29,60 +29,60 @@ def edge_validate(block_data):
   for r in range(0,3):
     for c in range(0,3):
       bomb_count2 = 0
-
-      #Check surrounding numbers for bombs  
+        
       #Ignore numbers outside of the grid
       if r < 2:
+        #Check surrounding numbers for bombs
         if block_data [r+1][c] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r+1, c)
+
       if r < 2 and c < 2:
         if block_data [r+1][c+1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r+1, c+1)
+
       if c < 2:
         if block_data [r][c+1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r, c+1)
+
       if r > 0 and c < 2:
         if block_data [r-1][c+1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r-1, c+1)
+
       if r > 0:
         if block_data [r-1][c] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r-1, c)
+
       if r > 0 and c > 0:
         if block_data [r-1][c-1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r-1, c-1)
+
       if c > 0:
         if block_data [r][c-1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r, c-1)
+
       if r < 2 and c > 0:
         if block_data [r+1][c-1] == -1:
           bomb_count2=bomb_count2+1
           print("coordinates ", r, ",", c, " found a bomb at ", r+1, c-1)
-      
-      #Check if surrounded number is valid
-      # if block_data [r][c] == bomb_count2:
-      #   c=c
-      #   print (bomb_count2)
-      #   print (r, c)
-      #If false, break loop and output invalid
+
       if block_data [r][c] != bomb_count2 and block_data [r][c] != -1:
-        print (bomb_count2)
-        print (r, c)
+        print ("the stated bomb count at", r, ",", c, "is", block_data[r][c], ". The number of bombs detected is", bomb_count2, ".")
         r = 10
         c = 10
         return "invalid input"
   return "valid input"
 
 grid = [
-  [1,2,-1],
-  [-1,3,2],
-  [1,2,-1]
+  [1,2,2],
+  [2,-1,-1],
+  [2,-1,3]
 ]
 
 print (edge_validate(grid))
